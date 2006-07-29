00315bd0467d0eb9090a2a2404c7362854721d34
Dean Brettle <dean@brettle.com>
2006-07-29 02:34:50 +0000

    Made assembly signing conditional on NEATUPLOAD_SIGNED symbol.  Added Signed Debug and Signed Release configurations which set that symbol.  This allows for day-to-day development and deployment in full-trust envs but also allows a strong name to be used for the final release.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -33,8 +33,10 @@ using System.IO;
 // The following attributes specify the key for the sign of your assembly. See the
 // .NET Framework documentation for more information about signing.
 // This is not required, if you don't want signing let these attributes like they're.
-[assembly: AssemblyDelaySign(true)]
-[assembly: AssemblyKeyFile("public.snk")]
+#if NEATUPLOAD_SIGNED 
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("../../../../keypair.snk")]
+#endif
 
 // This helps with VS designer support.
 [assembly: TagPrefix("Brettle.Web.NeatUpload", "Upload")]
