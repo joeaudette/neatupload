ab007e8bf06d4d5819badd3ab3e35244ae488f7d
Dean Brettle <dean@brettle.com>
2007-02-15 21:24:52 +0000

    Fixed bug where ProgressPage threw a NullReferenceException if the UploadContext for the lastPostBackID was not available (e.g. due to a server restart).

--- DetailsControl.cs
+++ DetailsControl.cs
@@ -78,7 +78,7 @@ namespace Brettle.Web.NeatUpload
 				if (lastPostBackID != null && lastPostBackID.Length > 0 && Page.Request.Params["refresher"] == null)
 				{
 					uploadContext = UploadContext.FindByID(lastPostBackID);
-					if (uploadContext.FileBytesRead == 0)
+					if (uploadContext != null && uploadContext.FileBytesRead == 0)
 					{
 						uploadContext = null;
 					}
