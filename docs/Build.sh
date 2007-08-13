c990691c7b65dc4494b637cc1c2da8a0603e0858
Dean Brettle <dean@brettle.com>
2007-08-13 07:12:11 +0000

    Switched from odt (compressed) to flatxml (uncompressed) format to help with version control.

--- docs/Build.sh
+++ docs/Build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-ooffice -invisible 'macro:///Standard.Module1.SaveAsHTML("'`pwd`'/Manual.odt")' || (echo "ERROR: Unable to build Manual.html" ; exit 1)
+echo "Converting Manual.flatxml to Manual.html via OpenOffice..."
+ooffice -invisible 'macro:///Standard.Module1.SaveAsHTML("'`pwd`'/Manual.flatxml")' || (echo "ERROR: Unable to build Manual.html" ; exit 1)
