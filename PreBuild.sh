98b36c7e3a21d5de602a0900e5ad759933210e81
Dean Brettle <dean@brettle.com>
2008-04-18 22:49:20 +0000

    Changed to use resgen1 instead of resgen, if available.

--- PreBuild.sh
+++ PreBuild.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 cd `dirname "$0"`
 find . -name "*.resources" -exec /bin/rm -f {} \;
-resgen /compile `find . -name "*.resx"`
+
+RESGEN=resgen
+if which resgen1 >/dev/null 2>&1; then
+	RESGEN=resgen1
+fi
+
+$RESGEN /compile `find . -name "*.resx"`
 
 for f in `find . -name "*.resources" -print`; do
 	mv -f $f NeatUpload.`echo $f | sed -e 's?^./??' -e 's?/?.?g'`
