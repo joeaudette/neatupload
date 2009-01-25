6e47f89efe5432ba74823aa302a846e1a6509e1f
Dean Brettle <dean@brettle.com>
2009-01-25 00:28:23 +0000

    * NeatUpload-1.3/dotnet/deploy/PostBuild: * NeatUpload-1.3/dotnet/deploy/PostBuild.bat: * NeatUpload-1.3/dotnet/src/Brettle.Web.NeatUpload/Brettle.Web.NeatUpload.csproj: * NeatUpload-1.3/dotnet/src/Brettle.Web.NeatUpload/NeatUpload.Strings.en-US.resx:   Added support for building/installing satellite assemblies for other   locales.

--- dotnet/deploy/PostBuild.bat
+++ dotnet/deploy/PostBuild.bat
@@ -8,3 +8,4 @@ copy "%1\..\..\flash\src\SWFUpload\install\*.*" "%2\NeatUpload\"
 copy "%1\..\src\Brettle.Web.NeatUpload\install\NeatUpload\*.*" "%2\NeatUpload\"
 copy "%1\bin\*.pdb" "%2\bin"
 copy "%1\bin\*.dll" "%2\bin"
+xcopy "%1\..\src\Brettle.Web.NeatUpload\install\bin" "%2\bin\" /s
