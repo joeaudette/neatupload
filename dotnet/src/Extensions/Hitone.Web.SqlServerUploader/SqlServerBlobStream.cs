8fb4755645798072e66a7ef8bfc9cabfa956d5a3
Dean Brettle <dean@brettle.com>
2009-03-09 02:26:27 +0000

    Fixed bug where SqlServerUploader would fail when using SQLServer 2005 without stored procs.

--- dotnet/src/Extensions/Hitone.Web.SqlServerUploader/SqlServerBlobStream.cs
+++ dotnet/src/Extensions/Hitone.Web.SqlServerUploader/SqlServerBlobStream.cs
@@ -107,6 +107,28 @@ namespace Hitone.Web.SqlServerUploader
 
         }
 
+        private static int _ServerMajorVersion = -1;
+        private int ServerMajorVersion
+        {
+            get
+            {
+                if (_ServerMajorVersion == -1)
+                {
+                    SqlCommand sqlCommand = _connection.CreateCommand();
+                    sqlCommand.CommandText = "SELECT SERVERPROPERTY('productversion')";
+                    string productVersion = null;
+                    _connection.Open();
+                    try
+                    {
+                        productVersion = sqlCommand.ExecuteScalar() as string;
+                    }
+                    finally { _connection.Close(); }
+                    _ServerMajorVersion = int.Parse(productVersion.Split('.')[0]);
+                }
+                return _ServerMajorVersion;
+            }
+        }
+
         /// <summary>
         /// Opens an exising blob for read/write access
         /// </summary>
@@ -127,8 +149,10 @@ namespace Hitone.Web.SqlServerUploader
             else //We should generate SQL Queries
             {
                 if (openProcedure != null) openProcedure = null; //Make sure the openProcedure parameter is null so future tests are simpler
-
-                openSqlCommand.CommandText = string.Format("SELECT @Pointer = TEXTPTR([{0}]), @Size=datalength([{0}]){2}{3} FROM [{1}] WHERE $IDENTITY = @Identity", dataColumnName, tableName,
+                // Only use TEXTPTR() with SqlServer 2000 and below.  We don't need the @Pointer value in later versions, so we just use an
+                // empty (but non-null) byte array.
+                string pointerValue = ServerMajorVersion < 9 ? string.Format("TEXTPTR([{0}])", dataColumnName) : "0x";
+                openSqlCommand.CommandText = string.Format("SELECT @Pointer = {0}, @Size=datalength([{1}]){3}{4} FROM [{2}] WHERE $IDENTITY = @Identity", pointerValue, dataColumnName, tableName,
                     fileNameColumnName != null ? ",@FileName=" + fileNameColumnName : string.Empty,
                     MIMETypeColumnName != null ? ",@MIMEType=" + MIMETypeColumnName : string.Empty);
             }
@@ -207,7 +231,10 @@ namespace Hitone.Web.SqlServerUploader
             createCommand.Parameters["@Bytes"].Size = 0;
             createCommand.Parameters["@Bytes"].Offset = 0;
 
-            sql.AppendFormat(");SELECT @Identity = SCOPE_IDENTITY(); SELECT @Pointer = TEXTPTR([{0}]) FROM [{1}] WHERE $IDENTITY = @Identity", dataColumnName, tableName);
+            // Only use TEXTPTR() with SqlServer 2000 and below.  We don't need the @Pointer value in later versions, so we just use an
+            // empty (but non-null) byte array.
+            string pointerValue = ServerMajorVersion < 9 ? string.Format("TEXTPTR([{0}])", dataColumnName) : string.Format("[{0}]", dataColumnName);
+            sql.AppendFormat(");SELECT @Identity = SCOPE_IDENTITY(); SELECT @Pointer = {0} FROM [{1}] WHERE $IDENTITY = @Identity", pointerValue, tableName);
 
             createCommand.CommandText = sql.ToString();
 
@@ -237,8 +264,15 @@ namespace Hitone.Web.SqlServerUploader
                 if (readProcedure != null && readProcedure.Length > 0) {
                     _readCommand.CommandType = CommandType.StoredProcedure;
                     _readCommand.CommandText = readProcedure;
-                } else
+                }
+                else if (ServerMajorVersion >= 9)// SQLServer 2005+
+                {
+                    _readCommand.CommandText = string.Format("SELECT SUBSTRING([{1}], @Offset+1, @Size) FROM [{0}] WHERE ($IDENTITY = @Identity)", tableName, dataColumnName);
+                }
+                else
+                {
                     _readCommand.CommandText = string.Format("READTEXT [{0}].[{1}] @Pointer @Offset @Size", tableName, dataColumnName);
+                }
 
                 //@Identity is not used by the created code, but may be used by the procedure (if they choose to go with the "SUBSTRING"-approach)
                 AddWithValue(_readCommand.Parameters, "@Identity", _identity);
@@ -279,8 +313,14 @@ namespace Hitone.Web.SqlServerUploader
                     _writeCommand.CommandType = CommandType.StoredProcedure;
                     _writeCommand.CommandText = writeProcedure;
                 }
-                else
+                else if (ServerMajorVersion >= 9) // SQLServer 2005+
+                {
+                    _writeCommand.CommandText = string.Format("UPDATE [{0}] SET [{1}].write(@Bytes, null, null) WHERE $IDENTITY = @Identity", tableName, dataColumnName);
+                }
+                else // SQLServer 2000
+                {
                     _writeCommand.CommandText = string.Format("UPDATETEXT [{0}].[{1}] @Pointer @Offset @Delete WITH LOG @Bytes", tableName, dataColumnName);
+                }
 
                 //@Identity is not used by the created code, but may be used by the procedure (if they choose to go with the ".WRITE"-approach)
                 AddWithValue(_writeCommand.Parameters, "@Identity", _identity);
