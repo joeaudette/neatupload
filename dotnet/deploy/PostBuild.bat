f9b2f302b36c3ff31c0c549e2eeee34bedf1866b
Dean Brettle <dean@brettle.com>
2009-11-19 00:00:23 -0800

    Create app/bin and app/NeatUpload so that deploy PostBuild script works.

--- dotnet/deploy/PostBuild.bat
+++ dotnet/deploy/PostBuild.bat
@@ -2,6 +2,7 @@ del "%1\bin\SWFUpload.*"
 del "%1\bin\js.*"
 del "%1\bin\deploy.*"
 md "%2\NeatUpload"
+md "%2\bin"
 
 copy "%1\..\..\js\install\*.js" "%2\NeatUpload\"
 copy "%1\..\..\flash\src\SWFUpload\install\*.*" "%2\NeatUpload\"
